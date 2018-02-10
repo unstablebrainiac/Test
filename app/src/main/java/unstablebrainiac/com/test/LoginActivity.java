6c1bc99d778098d7d43868a9e4f829588c1e63ce
Sajal Narang <sajalnarang@gmail.com>
2018-02-10 12:49:36 +0530

    Initial commit

---
+++ app/src/main/java/unstablebrainiac/com/test/LoginActivity.java
@@ -0,0 +1,14 @@
+package unstablebrainiac.com.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
