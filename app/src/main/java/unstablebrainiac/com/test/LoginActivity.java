c5dea47a7c4847d16789341025056e87262d9612
Sajal Narang <sajalnarang@gmail.com>
2018-02-10 16:09:08 +0530

    Change activity

--- app/src/main/java/unstablebrainiac/com/test/LoginActivity.java
+++ app/src/main/java/unstablebrainiac/com/test/LoginActivity.java
@@ -1,8 +1,13 @@
 package unstablebrainiac.com.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText emailEditText = findViewById(R.id.email_edit_text);
+        final EditText passwordEditText = findViewById(R.id.password_edit_text);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (email.equals("example@wncc-iitb.org") && password.equals("test")) {
+                    Toast.makeText(getApplicationContext(), "Login Seccessful", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
